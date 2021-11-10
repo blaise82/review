981056d7541840fd1d5fa93489eeacdce9d145c2
Blaise Izabbayo <52036624+blaise82@users.noreply.github.com>
2021-11-10 14:11:42 +0200

    Create mathclass.js

---
+++ mathclass.js
@@ -0,0 +1,21 @@
+class mathalg {
+  constructor(arr) {
+    this.arr = arr;
+  }
+
+  sum() {
+    let total_sum = 0;
+    for (let i = 0; i < this.arr.length; i++) {
+      total_sum += this.arr[i];
+    }
+    return total_sum;
+
+    // return this.arr.reduce((a, b) => a + b, 0);
+  }
+}
+
+const ARRAY = [1, 2, 3, 4, 5];
+
+const mathhandler = new mathalg([1, 2, 3, 4, 5]);
+
+console.log(mathhandler.sum());
